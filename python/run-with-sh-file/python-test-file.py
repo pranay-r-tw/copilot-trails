a9144501ac0dd9061a66aadbc5d624f4b8d9bc00
Pranay R <pranay.r@thoughtworks.com>
2023-07-12 21:03:49 +0530

    python test

---
+++ python/run-with-sh-file/python-test-file.py
@@ -0,0 +1,5 @@
+# take input from user
+name = input("Enter your name: ")
+
+# display the name along with welcome message
+print("Welcome", name)
---
+++ python/run-with-sh-file/run-py-file.sh
@@ -0,0 +1,2 @@
+# run python file in this folder
+python3 python-test-file.py
